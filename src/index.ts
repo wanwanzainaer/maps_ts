c63785183728a1ecfa91a81c8cc2d804ff1ab5a9
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-06 23:35:04 -0800

    Add google map types and div

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ import { User } from './User';
 const user = new User();
 
 console.log(user);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
